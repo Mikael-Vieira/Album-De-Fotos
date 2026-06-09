f2129164cf70dd1f172dcab6516947d2211d2f82
Mikael-Vieira <mikaelvcasoti@gmail.com>
2026-06-09 13:50:53 -0300

    designer dos botoes de editar o album

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,30 +1,36 @@
 @extends('layout.app')
 
 @section('content')
-    <div class="home-header">
-        <h1>Meus Álbuns</h1>
-        <p>Organize suas fotos em coleções personalizadas.</p>
-    </div>
+<div class="home-header">
+    <h1>Meus Álbuns</h1>
+    <p>Organize suas fotos em coleções personalizadas.</p>
+</div>
 
-    <div class="albums-container">
+<div class="albums-container">
 
-        @foreach ($albums as $album)
-            <a href="{{ route('albums.show', $album->id) }}" class="album-card-link" style="text-decoration: none; color: inherit;">
-                <div class="album-card">
-                    <div class="album-image">
-                        @if ($album->cover_photo)
-                            <img src="{{ asset('storage/' . $album->cover_photo) }}">
-                        @else
-                            <img src="https://placehold.co/400x250?text=Sem+Capa">
-                        @endif
-                    </div>
-                    <div class="album-info">
-                        <h3>{{ $album->name }}</h3>
-                        <p>{{ $album->description }}</p>
-                    </div>
+    @foreach ($albums as $album)
+    <a href="{{ route('albums.show', $album->id) }}" class="album-card-link" style="text-decoration: none; color: inherit;">
+        <div class="album-card">
+            <div class="album-image">
+                @if ($album->cover_photo)
+                <img src="{{ asset('storage/' . $album->cover_photo) }}">
+                @else
+                <img src="https://placehold.co/400x250?text=Sem+Capa">
+                @endif
+            </div>
+            <div class="album-info">
+                <h3>{{ $album->name }}</h3>
+                <p>{{ $album->description }}</p>
+
+                <!-- Tenho que adicionar a funcionalidade desse botao -->
+                <div class="album-actions">
+                    <button class="btn-editar-album"> Editar </button>
                 </div>
-            </a>
-        @endforeach
 
-    </div>
+            </div>  
+        </div>
+    </a>
+    @endforeach
+
+</div>
 @endsection
